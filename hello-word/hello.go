c81e5c86ff39e5e7a4197bd04af98c25581e0677
jpcadinelli <jpcadinelli@gmail.com>
2024-03-17 09:16:46 -0300

    Aula 4 finalizada

--- hello-word/hello.go
+++ hello-word/hello.go
@@ -2,25 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 	exibeIntrodução()
-	exibeMenu()
-	comando := leComando()
-
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando.")
-	case 2:
-		fmt.Println("Exibindo Logs.")
-	case 0:
-		fmt.Println("Saindo do programa.")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando.")
-		os.Exit(-1)
+	for {
+		exibeMenu()
+		comando := leComando()
+	
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando.")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -42,4 +45,15 @@ func leComando() int {
 	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br"
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
