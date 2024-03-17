f3f961cd69afebae6c6f424b276b74e85824e136
jpcadinelli <jpcadinelli@gmail.com>
2024-03-17 15:31:02 -0300

    Curso finalizado

--- hello-word/hello.go
+++ hello-word/hello.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -27,6 +29,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa...")
 			os.Exit(0)
@@ -70,8 +73,10 @@ func iniciarMonitoramento() {
 			}
 			if resp.StatusCode == 200 {
 				fmt.Println("Site:", sites[i], "foi carregado com sucesso!")
+				registraLog(sites[i], true)
 			} else {
 				fmt.Println("Site:", sites[i], "está com problemas. Status Code:", resp.StatusCode)
+				registraLog(sites[i], false)
 			}
 		}
 		fmt.Println("")
@@ -87,13 +92,30 @@ func leSitesDoArquivo() []string {
 	}
 	leitor := bufio.NewReader(arquivo)
 	for {
-        linha, err := leitor.ReadString('\n')
+		linha, err := leitor.ReadString('\n')
 		linha = strings.TrimSpace(linha)
 		sites = append(sites, linha)
-        if err == io.EOF {
-            break
-        }
-    }
+		if err == io.EOF {
+			break
+		}
+	}
 	arquivo.Close()
 	return sites
+}
+
+func registraLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + "- online: " + strconv.FormatBool(status) + "\n")
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+	fmt.Println(string(arquivo))
 }
