140c27acf358a46c86ad681a343f2834ed81e7fb
jpcadinelli <jpcadinelli@gmail.com>
2024-03-19 16:38:19 -0300

    primeiro passo curso

--- gestao-contas-bank/main.go
+++ gestao-contas-bank/main.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	var titular string = "João Pedro"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float64 = 158.99
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
 }
