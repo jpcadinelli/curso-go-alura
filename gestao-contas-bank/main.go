06b25b201f1600a4c4717d83a32a69003e6ae248
jpcadinelli <jpcadinelli@gmail.com>
2024-03-20 17:44:49 -0300

    Aula 2 finalizada

--- gestao-contas-bank/main.go
+++ gestao-contas-bank/main.go
@@ -9,9 +9,20 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque <= c.saldo && valorDoSaque > 0
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso."
+	} else {
+		return "Saldo insuficiente."
+	}
+}
+
 func main() {
 	contaDoJoao := ContaCorrente{titular: "João Pedro", numeroAgencia: 589, numeroConta: 123456, saldo: 158.99}
-	contaDaBruna := ContaCorrente{titular: "Bruna", numeroAgencia: 589, numeroConta: 654321, saldo: 1345.00}
-	fmt.Println(contaDoJoao)
-	fmt.Println(contaDaBruna)
+	fmt.Println(contaDoJoao.saldo)
+	fmt.Println(contaDoJoao.Sacar(-100))
+	fmt.Println(contaDoJoao.saldo)
 }
