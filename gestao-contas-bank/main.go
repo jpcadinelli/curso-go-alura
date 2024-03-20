40591adfcb33d095bb63c0a9834c449dec6921e1
jpcadinelli <jpcadinelli@gmail.com>
2024-03-20 17:51:48 -0300

    parcial da aula 3

--- gestao-contas-bank/main.go
+++ gestao-contas-bank/main.go
@@ -10,19 +10,28 @@ type ContaCorrente struct {
 }
 
 
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
 	podeSacar := valorDoSaque <= c.saldo && valorDoSaque > 0
 	if podeSacar {
 		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso."
+		return "Saque realizado com sucesso.", c.saldo
 	} else {
-		return "Saldo insuficiente."
+		return "Saldo insuficiente.", c.saldo
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	podeDepositar := valorDoDeposito > 0
+	if podeDepositar {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso.", c.saldo
+	} else {
+		return "Valor negativo não pode ser depositado.", c.saldo
 	}
 }
 
 func main() {
 	contaDoJoao := ContaCorrente{titular: "João Pedro", numeroAgencia: 589, numeroConta: 123456, saldo: 158.99}
 	fmt.Println(contaDoJoao.saldo)
-	fmt.Println(contaDoJoao.Sacar(-100))
-	fmt.Println(contaDoJoao.saldo)
+	fmt.Println(contaDoJoao.Depositar(100))
 }
