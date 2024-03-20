b522161162537a98a2d5004f4790d39e0f50ee1a
jpcadinelli <jpcadinelli@gmail.com>
2024-03-20 17:25:09 -0300

    Aula 1 finalizada

--- gestao-contas-bank/main.go
+++ gestao-contas-bank/main.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
 func main() {
-	var titular string = "João Pedro"
-	var numeroAgencia int = 589
-	var numeroConta int = 123456
-	var saldo float64 = 158.99
-	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+	contaDoJoao := ContaCorrente{titular: "João Pedro", numeroAgencia: 589, numeroConta: 123456, saldo: 158.99}
+	contaDaBruna := ContaCorrente{titular: "Bruna", numeroAgencia: 589, numeroConta: 654321, saldo: 1345.00}
+	fmt.Println(contaDoJoao)
+	fmt.Println(contaDaBruna)
 }
